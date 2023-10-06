5532702b52baa375c0cbc5531287e9eb6a86a60c
Nikhil758 <111441525+Nikhil758@users.noreply.github.com>
2023-10-06 09:30:15 +0530

    Add files via upload

---
+++ nt.js
@@ -0,0 +1,40 @@
+function first(){
+    document.getElementById('imgg')
+    .src="pics/n5.jpg";
+    document.getElementById('aa').innerHTML="Truly authentic sound";
+    document.getElementById('bb').innerHTML="When tuning Ear (2), we innovated. Perfecting the path from artist to ear. So that music sounds just as it was intended. A new dual chamber design and our 11.6 mm dynamic driver ensures playback reveals every detail. Transporting you to the recording studio, wherever you are.";
+}
+     
+function second(){
+    document.getElementById('imgg')
+    .src="pics/n3.jpg";
+    document.getElementById('aa').innerHTML="Hi-Res Audio Certified";
+    document.getElementById('bb').innerHTML="Authentic on every level. Ear (2) is Hi-Res Audio certified for playback upto 1 Mbps. And frequencies up to 24bit/192 kHz. Sound details are protected. Supports LHDC 5.0 codec for high-resolution streaming over bluetooth. For powerfully pure audio with no quality damage. just how the artist intended.";
+}
+
+function third(){
+    document.getElementById('imgg')
+    .src="pics/n6.jpg";
+    document.getElementById('aa').innerHTML="Studio quality";
+    document.getElementById('bb').innerHTML="Studio quality sound anywhere. Our powerful 11.6 mm driver brings you effortlessly powerful bass and even richer high notes. Coupled with an all-new dual chamber design for smoother air flow. Giving brighter, clearer audio. Wonderfully true-to-life";
+}
+function f(){
+    document.getElementById('i')
+    .src="pics/nt.jpg";
+    document.getElementById('b')
+    .href="https://www.flipkart.com/nothing-ear-2-dual-chamber-sound-hi-res-audio-smart-anc-connection-bluetooth-headset/p/itm6d79168be59b2?pid=ACCGQFSYJMHPNFEN&lid=LSTACCGQFSYJMHPNFENYB5EX1&marketplace=FLIPKART&srno=s_1_2&otracker=AS_Query_OrganicAutoSuggest_8_7_na_na_na&otracker1=AS_Query_OrganicAutoSuggest_8_7_na_na_na&fm=search-autosuggest&iid=00dfb069-a2c1-49f5-ad48-ee50226ecd7b.ACCGMW9ZZG2ADGSQ.SEARCH&ppt=sp&ppn=sp&qH=273a048e607ada91";
+    document.getElementById('c1')
+    .style="background-color: #000000; color:#fff";
+    document.getElementById('c2')
+    .style="background-color: #fff; color:#000000";
+}
+function s(){
+    document.getElementById('i')
+    .src="pics/nw.jpg";
+    document.getElementById('b')
+    .href="https://www.flipkart.com/nothing-ear-2-dual-chamber-sound-hi-res-audio-smart-anc-connection-bluetooth-headset/p/itmd0fdab5d8d2f6?pid=ACCGMW9ZZG2ADGSQ&lid=LSTACCGMW9ZZG2ADGSQ0O2CJ9&marketplace=FLIPKART&q=nothing+ear+2&store=0pm%2Ffcn&spotlightTagId=FkPickId_0pm%2Ffcn&srno=s_1_2&otracker=AS_Query_OrganicAutoSuggest_8_7_na_na_na&otracker1=AS_Query_OrganicAutoSuggest_8_7_na_na_na&fm=search-autosuggest&iid=00dfb069-a2c1-49f5-ad48-ee50226ecd7b.ACCGMW9ZZG2ADGSQ.SEARCH&ppt=sp&ppn=sp&qH=273a048e607ada91";
+    document.getElementById('c1')
+    .style="background-color: #fff; color:#000000";
+    document.getElementById('c2')
+    .style="background-color: #000000; color:#fff";
+}
